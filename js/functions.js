9d06a867630355a86817fbfd554c6ce14a4ad614
Chenry <littlechenry@gmail.com>
2018-04-18 15:08:36 +0800

    邀请信息

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 function userInfo() {
 	var title = "用户基本信息";
-	var tableth = new Array("用户id","服务器id","手机号","注册时间","注册ip","昵称","红包","余额","设置");
+	var tableth = new Array("用户id","服务器id","手机号","注册时间","注册ip","昵称","红包/￥","余额/￥","设置");
 	contentMain(title,tableth);
 	var pages = '<a class="pages-a"><i class="fa fa-angle-double-left"></i></a>'
 		+ '<a class="pages-a current">1</a><a class="pages-a">2</a><span>……</span>'
@@ -11,29 +11,21 @@ function userInfo() {
 
 function invitation() {
 	var title = "邀请统计";
-	var tableth = new Array("层级","邀请数","有效客户","已奖励数","操作");
+	var tableth = new Array("用户","邀请数/人","有效用户/人","充值总额/￥","消费（新充值部分）/￥","消费总额/￥","应发红包/￥");
 	contentMain(title,tableth);
-	$(".advance-area").prepend('<div class="param"></div>');
-	$(".advance-area").prepend('<div class="param1"></div>');
-	
-	$(".param1").append('<span>有效消费总额：</span><input id="validspending" type="text" class="" >');
-	$(".param1").append('<span>有效消费余额：</span><input id="validspendingcharge" type="text" class="" >');
-	$(".param1").append('<span>开始时间：</span><input id="starttime" type="date" class="" >');
-	$(".param1").append('<span>结束时间：</span><input id="endtime" type="date" class="" >');
-	$(".param").append('<span>用户总数权重：</span><input id="totaluser" type="text" class="" >');
-	$(".param").append('<span>有效用户权重：</span><input id="validuser" type="text" class="" >');
-	$(".param").append('<span>充值总额权重：</span><input id="Totalrecharge" type="text" class="" >');
-	$(".param").append('<span>消费总额权重：</span><input id="Totalspending" type="text" class="" >');
-	$(".param").append('<span>消费充值总额权重：</span><input id="Totalspendingcharge" type="text" class="" >');
-	$(".param").append('<button id="submit" type="button" class="">提交</button>');
+	$(".filter span").after('<ul></ul>');
+	$(".filter ul").append('<li><span>查询时间范围：<input id="starttime" type="datetime-local" class="" > - <input id="endtime" type="datetime-local" class="" ></span></li>');
+	$(".filter ul").append('<li><span>有效消费总额≥<input id="validspending" type="number" class="" placeholder="￥"></span><span>有效消费余额≥<input id="validspendingcharge" type="number" class="" placeholder="￥"></span></li>');
+	$(".filter ul").append('<li><span>用户总数权重 = <input id="totaluser" type="number" class="" value=0.1></span><span>有效用户权重 = <input id="validuser" type="number" class="" value=0.1></span><span>充值总额权重 = <input id="Totalrecharge" type="number" class="" value=0.1></span><span>消费充值总额权重 = <input id="Totalspendingcharge" type="number" class="" value=0.1></span><span>消费总额权重 = <input id="Totalspending" type="number" class="" value=0.1></span></li>');
 	$("#output").after('<button id="refresh" type="button" class="">刷新数据</button>');
 	$("#query").unbind("click").bind("click",function(){
-		queryinvite($("#search").val());
-	});	
-	$("#submit").unbind("click").bind("click",function(){
-		submitparam();
+		queryInvite($("#search").val());
 	});
-	
+	$("#validspending").val(0);
+	$("#validspendingcharge").val(0);
+	var currentDate = new Date();
+	$("#starttime").val(currentDate.Format("yyyy-MM-dd") + "T00:00");
+	$("#endtime").val(currentDate.Format("yyyy-MM-dd") + "T23:59");
 }
 
 function defaultFunction() {
@@ -46,15 +38,15 @@ function contentMain(title,tableth) {
 	var content = $(".content");
 	content.html("");
 	var titlecontent = '<div id="pagetitle" class="subtitle">'+ title +'</div>';
-	var advancecontent = '<div class="search-area"><input type="text" id="search" class=""><button id="multiinput" type="button" class="">'
-		+ '批量导入</button><button id="advance" type="button" class="">高级选项</button><button id="query" type="button" class="">'
+	var advancecontent = '<div class="search-area"><input type="text" id="search" class="" placeholder="输入手机号码，以;分隔"><button id="multiinput" type="button" class="">'
+		+ '批量导入</button><button id="advance" type="button" class="">高级选项<i class="fa fa-angle-double-up"></i></button><button id="query" type="button" class="" style="margin-right:40px;">'
 		+ '查询结果</button><button type="button" id="output" class="">导出Excel</button><div class="advance-area">'
 		+ '<div class="filter"><span>筛选：</span></div><div class="sort"><span>排序：</span></div></div></div>';
-	var resultcontent = '<div class="results"><div class="datatable"><div class="pages"></div><div><table class="table" id="table" ><tr>';
+	var resultcontent = '<div class="results"><div class="datatable"><div class="pages"></div><div><table class="table" id="table" ><thead><tr>';
 	for (var i = 0; i < tableth.length; i++) {
-		resultcontent = resultcontent + '<th>'+ tableth[i] +'</th>';
+		resultcontent = resultcontent + '<th title='+ tableth[i] +'>'+ tableth[i] +'</th>';
 	}
-	resultcontent	+= '</tr></table></div></div></div></div>';
+	resultcontent += '</tr></thead></table></div></div></div></div>';
 	content.append(titlecontent);
 	content.append(advancecontent);
 	content.append(resultcontent);	
@@ -64,6 +56,7 @@ function contentMain(title,tableth) {
 function actionInit() {
 	$("#advance").unbind("click").bind("click",function(){
 		$(".advance-area").slideToggle();
+		$(this).find("i").toggleClass("fa-angle-double-up fa-angle-double-down");
 	});
 	$("#multiinput").unbind("click").bind("click",function(){
 		$(".modal").fadeIn();
@@ -76,75 +69,67 @@ function actionInit() {
 	});
 }
 
-function queryinvite(phone){
-	var list=phone.split(";");	
-	var validspending=$("#validspending").val();
-	var validspendingcharge=$("#validspendingcharge").val();
-	var totaluser=$("#totaluser").val();
-	var validuser=$("#validuser").val();
-	var starttime=$("#starttime").val();
-	var endtime=$("#endtime").val();
-	
-	var startdate=new Date(starttime);
-	var enddate=new Date(endtime);
-	starttime=startdate.getTime()/1000;
-	endtime=enddate.getTime()/1000;
-	var Totalrecharge=$("#Totalrecharge").val();
-	var Totalspending=$("#Totalspending").val();
-	var Totalspendingcharge=$("#Totalspendingcharge").val();
+function queryInvite(phone) {
+	if ($("#validspending").val() == "" || $("#validspendingcharge").val() == "" || phone == "") {
+		console.log("有空未填");
+		return false;
+	}
+	var list = phone.split(";");
 	console.log(list);
+	var validspending = parseInt($("#validspending").val()).toFixed(2) * 100;
+	var validspendingcharge = parseInt($("#validspendingcharge").val()).toFixed(2) * 100;
+	var totaluser = $("#totaluser").val();
+	var validuser = $("#validuser").val();
+	var starttime = $("#starttime").val();
+	var endtime = $("#endtime").val();
+	console.log(starttime);
+	var startdate = new Date(starttime);
+	var enddate = new Date(endtime);
+	enddate = new Date(enddate.setDate(enddate.getDate() + 1));
+	console.log(enddate);
+	starttime = startdate.getTime() / 1000;
+	endtime = enddate.getTime() / 1000;
+	var Totalrecharge = $("#Totalrecharge").val();
+	var Totalspending = $("#Totalspending").val();
+	var Totalspendingcharge = $("#Totalspendingcharge").val();
 	$.ajax({
 			type: "get",
-			async: false,
+			async: true,
 			url: "/invite/redEnvelope",
 			data:{
-				phone:list,
-				totalConsume:validspending,
-				chargeConsume:validspendingcharge,
-				startTime:starttime,
-				endTime:endtime
+				phone : phone,
+				totalConsume : validspending,
+				chargeConsume : validspendingcharge,
+				startTime : starttime,
+				endTime : endtime
 			},
 			dataType: "json",
 			success: function (data) {
-				// var table=$("table");
-				// var tbody="<tbody>";
-				// for (var i = 0; i < data.data.length; i++) {
-				// 	var tr = '<tr><td>'+i+'</td><td>'+ data.data[i].total +'</td><td>'+data.data[i].valid+'</td><td></td><td></td></tr>'
-				// 	tbody += tr;
-				// }
-				// tbody += "</tbody>";
-				// table.append(tbody);
-				alert(data.data);
+				console.log(data);
+				var table=$("table");
+				table.find("tbody").remove();
+				var tbody="<tbody>";
+				for (var i = 0; i < data.data.length; i++) {
+					var newCustomer = parseInt(data.data[i].newCustomer);
+					var newValidCustomer = parseInt(data.data[i].newValidCustomer);
+					var newTotalCharge = parseInt(data.data[i].newTotalCharge) / 100;
+					var newTotalConsumeFromCharge = parseInt(data.data[i].newTotalConsumeFromCharge)/100;
+					var newTotalConsume = parseInt(data.data[i].newTotalConsume) / 100;
+					var C_newCustomer = $("#totaluser").val();
+					var C_newValidCustomer = $("#validuser").val();
+					var C_newTotalCharge = $("#Totalrecharge").val();
+					var C_newTotalConsumeFromCharge = $("#Totalspendingcharge").val();
+					var C_newTotalConsume = $("#Totalspending").val();
+					var redpacket = newCustomer * C_newCustomer + newValidCustomer * C_newValidCustomer + newTotalCharge * C_newTotalCharge + newTotalConsumeFromCharge * C_newTotalConsumeFromCharge + newTotalConsume * C_newTotalConsume;
+					var tr = '<tr><td>'+ data.data[i].phone +'</td><td>'+ newCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newTotalConsumeFromCharge +'</td><td>'+ newTotalConsume +'</td><td>'+ redpacket +'</td></tr>';
+					tbody += tr;
+				}
+				tbody += "</tbody>";
+				table.append(tbody);
 			}
 		});
 }
 
-// function submitparam(){	
-// 	var validspending=$("#validspending").val();
-// 	var validspendingcharge=$("#validspendingcharge").val();
-// 	var totaluser=$("#totaluser").val();
-// 	var validuser=$("#validuser").val();
-// 	var starttime=$("#starttime").val();
-// 	var endtime=$("#endtime").val();
-// 	var Totalrecharge=$("#Totalrecharge").val();
-// 	var Totalspending=$("#Totalspending").val();
-// 	var Totalspendingcharge=$("#Totalspendingcharge").val();
-// 	$.ajax({
-// 			type: "get",
-// 			async: false,
-// 			url: "/invite/ ",
-// 			data:{
-// 				totalConsume:validspending,
-// 				chargeConsume:validspendingcharge,
-// 				startTime:starttime,
-// 				endTime:endtime				
-// 			},
-// 			dataType: "json",
-// 			success: function (data) {
-// 				alert("提交成功");
-// 			}
-// 		});
-// }
 var tableToExcel = (function() {
     var uri = 'data:application/vnd.ms-excel;base64,',
     template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" '
@@ -159,5 +144,5 @@ var tableToExcel = (function() {
                 if (!table.nodeType) table = document.getElementById("table")
                 var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
                 window.location.href = uri + base64(format(template, ctx))
-              }
+        	}
 })();
