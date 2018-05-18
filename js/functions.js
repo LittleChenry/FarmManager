8441c3bad20a83402eba9131647bc737d6d9b192
jy <2529069834@qq.com>
2018-05-18 09:45:14 +0800

    jy

--- js/functions.js
+++ js/functions.js
@@ -11,12 +11,12 @@ function userInfo() {
 
 function invitation() {
 	var title = "邀请统计";
-	var tableth = new Array("用户","邀请数/人","有效用户/人","充值总额/￥","消费（新充值部分）/￥","消费总额/￥","应发红包/￥");
+	var tableth = new Array("用户","级数","邀请数/人","有效用户/人","消费总额/￥","应发奖励/￥","应发总奖励/￥");
 	contentMain(title,tableth);
 	$(".filter span").after('<ul></ul>');
-	$(".filter ul").append('<li><span>查询时间范围：<input id="starttime" type="datetime-local" class="" > - <input id="endtime" type="datetime-local" class="" ></span></li>');
-	$(".filter ul").append('<li><span>有效消费总额≥<input id="validspending" type="number" class="" placeholder="￥"></span><span>有效消费余额≥<input id="validspendingcharge" type="number" class="" placeholder="￥"></span></li>');
-	$(".filter ul").append('<li><span>用户总数权重 = <input id="totaluser" type="number" class="" value=0.1></span><span>有效用户权重 = <input id="validuser" type="number" class="" value=0.1></span><span>充值总额权重 = <input id="Totalrecharge" type="number" class="" value=0.1></span><span>消费充值总额权重 = <input id="Totalspendingcharge" type="number" class="" value=0.1></span><span>消费总额权重 = <input id="Totalspending" type="number" class="" value=0.1></span></li>');
+	$(".filter ul").append('<li><span>查询时间范围：<input id="starttime" type="datetime-local" class="" > - <input id="endtime" type="datetime-local" class="" ></span><span>有效消费总额≥<input id="validspending" type="number" class="" placeholder="￥"></span></li>');
+	//$(".filter ul").append('<li><span>有效消费总额≥<input id="validspending" type="number" class="" placeholder="￥"></span></li>');
+	$(".filter ul").append('<li><span>有效用户权重 = <input id="validuser" type="number" class="" value=0.1></span><span>消费总额权重 = <input id="Totalspending" type="number" class="" value=0.1></span><span>有效用户递减系数 = <input id="validuserreduce" type="number" class="" value=0.1></span><span>消费总额递减系数 = <input id="Totalspendingreduce" type="number" class="" value=0.1></span></li>');
 	$("#output").after('<button id="refresh" type="button" class="">刷新数据</button>');
 	$("#query").unbind("click").bind("click",function(){
 		queryInvite($("#search").val());
@@ -70,16 +70,14 @@ function actionInit() {
 }
 
 function queryInvite(phone) {
-	if ($("#validspending").val() == "" || $("#validspendingcharge").val() == "" || phone == "") {
+	if ($("#validspending").val() == "" || phone == "") {
 		console.log("有空未填");
 		return false;
 	}
 	var list = phone.split(";");
 	console.log(list);
 	var validspending = parseInt($("#validspending").val()).toFixed(2) * 100;
-	var validspendingcharge = parseInt($("#validspendingcharge").val()).toFixed(2) * 100;
-	var totaluser = $("#totaluser").val();
-	var validuser = $("#validuser").val();
+	
 	var starttime = $("#starttime").val();
 	var endtime = $("#endtime").val();
 	console.log(starttime);
@@ -89,47 +87,87 @@ function queryInvite(phone) {
 	console.log(enddate);
 	starttime = startdate.getTime() / 1000;
 	endtime = enddate.getTime() / 1000;
-	var Totalrecharge = $("#Totalrecharge").val();
-	var Totalspending = $("#Totalspending").val();
-	var Totalspendingcharge = $("#Totalspendingcharge").val();
+	// var Totalrecharge = $("#Totalrecharge").val();
+	// var Totalspending = $("#Totalspending").val();
+	// var Totalspendingcharge = $("#Totalspendingcharge").val();
 	$.ajax({
 			type: "get",
 			async: true,
-			url: "/invite/redEnvelope",
+			url: "statistics",
 			data:{
 				phone : phone,
-				totalConsume : validspending,
-				chargeConsume : validspendingcharge,
+				totalConsume : validspending,				
 				startTime : starttime,
 				endTime : endtime
 			},
 			dataType: "json",
 			success: function (data) {
 				console.log(data);
-				var table=$("table");
-				table.find("tbody").remove();
-				var tbody="<tbody>";
-				for (var i = 0; i < data.data.length; i++) {
-					var newCustomer = parseInt(data.data[i].newCustomer);
-					var newValidCustomer = parseInt(data.data[i].newValidCustomer);
-					var newTotalCharge = parseInt(data.data[i].newTotalCharge) / 100;
-					var newTotalConsumeFromCharge = parseInt(data.data[i].newTotalConsumeFromCharge)/100;
-					var newTotalConsume = parseInt(data.data[i].newTotalConsume) / 100;
-					var C_newCustomer = $("#totaluser").val();
-					var C_newValidCustomer = $("#validuser").val();
-					var C_newTotalCharge = $("#Totalrecharge").val();
-					var C_newTotalConsumeFromCharge = $("#Totalspendingcharge").val();
-					var C_newTotalConsume = $("#Totalspending").val();
-					var redpacket = newCustomer * C_newCustomer + newValidCustomer * C_newValidCustomer + newTotalCharge * C_newTotalCharge + newTotalConsumeFromCharge * C_newTotalConsumeFromCharge + newTotalConsume * C_newTotalConsume;
-					var tr = '<tr><td>'+ data.data[i].phone +'</td><td>'+ newCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newTotalConsumeFromCharge +'</td><td>'+ newTotalConsume +'</td><td>'+ redpacket +'</td></tr>';
-					tbody += tr;
-				}
-				tbody += "</tbody>";
-				table.append(tbody);
+				creatinvitetable(data);
+				console.log( Object.keys(data.data[0].level).length);
+			// 	var table=$("table");
+			// 	table.find("tbody").remove();
+			// 	var tbody="<tbody>";
+			// 	for (var i = 0; i < data.data.length; i++) {
+			// 		var newCustomer = parseInt(data.data[i].newCustomer);
+			// 		var newValidCustomer = parseInt(data.data[i].newValidCustomer);
+			// 		var newTotalCharge = parseInt(data.data[i].newTotalCharge) / 100;
+			// 		var newTotalConsumeFromCharge = parseInt(data.data[i].newTotalConsumeFromCharge)/100;
+			// 		var newTotalConsume = parseInt(data.data[i].newTotalConsume) / 100;
+			// 		var C_newCustomer = $("#totaluser").val();
+			// 		var C_newValidCustomer = $("#validuser").val();
+			// 		var C_newTotalCharge = $("#Totalrecharge").val();
+			// 		var C_newTotalConsumeFromCharge = $("#Totalspendingcharge").val();
+			// 		var C_newTotalConsume = $("#Totalspending").val();
+			// 		var redpacket = newCustomer * C_newCustomer + newValidCustomer * C_newValidCustomer + newTotalCharge * C_newTotalCharge + newTotalConsumeFromCharge * C_newTotalConsumeFromCharge + newTotalConsume * C_newTotalConsume;
+			// 		var tr = '<tr><td>'+ data.data[i].phone +'</td><td>'+ newCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newTotalConsumeFromCharge +'</td><td>'+ newTotalConsume +'</td><td>'+ redpacket +'</td></tr>';
+			// 		tbody += tr;
+			// 	}
+			// 	tbody += "</tbody>";
+			// 	table.append(tbody);
 			}
 		});
 }
-
+function creatinvitetable(data){
+	var table=$("table");
+	table.find("tbody").remove();
+	var tbody="<tbody>";
+	var total=new Array();
+	for (var i = 0; i < data.data.length; i++) {
+		total[i]=0;
+		var len=Object.keys(data.data[i].level).length;
+		var newCustomer = parseInt(data.data[i].phone);
+		var tr='<tr><td rowspan="'+(len-1)+'">'+ data.data[i].phone +'</td>';
+		for(var j = 1; j < len; j++){		
+		var newValidCustomer = parseInt(data.data[i].level[j].newValidCustomer);
+		var newTotalConsume = parseInt(data.data[i].level[j].newTotalConsume) / 100;
+		var totalCustomer = parseInt(data.data[i].level[j].totalCustomer);
+		var Totalspending = $("#Totalspending").val()*1.0;
+		var Totalspendingreduce = $("#Totalspendingreduce").val()*1.0;
+		var validuserreduce = $("#validuserreduce").val()*1.0;
+		var validuser = $("#validuser").val()*1.0;
+		var redpacket=newValidCustomer*validuser*Math.pow(validuserreduce,j-1)+newTotalConsume*Totalspending*Math.pow(Totalspendingreduce,j-1);
+		redpacket=redpacket.toFixed(2)*1.0;
+		total[i]+=redpacket;
+		if(j==1){
+			tr += '<td>'+ j +'</td><td>'+ totalCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newTotalConsume +'</td><td>'+ redpacket +'</td><td rowspan="'+(len-1)+'"></td></tr>';
+		}else{
+			tr='<tr><td>'+ j +'</td><td>'+ totalCustomer +'</td><td>'+ newValidCustomer +'</td><td>'+ newTotalConsume +'</td><td>'+ redpacket +'</td></tr>';
+		}
+		tbody += tr;
+		}
+	}
+	tbody += "</tbody>";
+	table.append(tbody);
+	table.attr("border","1");
+	var row=1;
+	for (var i = 0; i < data.data.length; i++) {
+		if(i>0){
+			row+=Object.keys(data.data[i-1].level).length-1;
+		}	
+		$("tr:eq("+row+") td:eq(6)").html(total[i].toFixed(2));
+	}
+}
 var tableToExcel = (function() {
     var uri = 'data:application/vnd.ms-excel;base64,',
     template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" '
